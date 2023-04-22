22d837b546eaa157c0bbef78d004a29b30131b14
FoRoB <andmyag190@yandex.ru>
2023-04-22 22:40:43 +0300

    added functionality of the sidebar

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -13,11 +13,30 @@ namespace Memento.ViewModels
     {
         public RelayCommand<IClosable> CloseWindowCommand { get; set; }
         public RelayCommand<IMinimizable> MinimizeWindowCommand { get; set; }
+        public RelayCommand ToHomeViewCommand { get; set; }
+        public RelayCommand ToFlashcardsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public FlashcardsViewModel FlashcardsVM { get; set; }
+
+        private object currentView;
+        public object CurrentView
+        {
+            get => currentView;
+            set => SetProperty(ref currentView, value);
+        }
 
         public MainWindowViewModel()
         {
-            CloseWindowCommand = new RelayCommand<IClosable>(x => x.Close());
-            MinimizeWindowCommand = new RelayCommand<IMinimizable>(x => x.Minimize());
+            CloseWindowCommand = new RelayCommand<IClosable>(x => x?.Close());
+            MinimizeWindowCommand = new RelayCommand<IMinimizable>(x => x?.Minimize());
+
+            HomeVM = new HomeViewModel();
+            FlashcardsVM = new FlashcardsViewModel();
+            CurrentView = HomeVM;
+
+            ToHomeViewCommand = new RelayCommand(() => CurrentView = HomeVM);
+            ToFlashcardsViewCommand = new RelayCommand(() => CurrentView = FlashcardsVM);
         }
     }
 }
