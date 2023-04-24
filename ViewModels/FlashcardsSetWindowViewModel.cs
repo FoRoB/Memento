9e0161f3478a3a33a97fe4a9aa73e10945b16976
FoRoB <andmyag190@yandex.ru>
2023-04-24 21:02:54 +0300

    created FlashcardsSet window

---
+++ ViewModels/FlashcardsSetWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Memento.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Memento.ViewModels
+{
+    class FlashcardsSetWindowViewModel
+    {
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand DragWindowCommand { get; set; }
+
+        public FlashcardsSetWindowViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<IClosable>(x => x?.Close());
+            DragWindowCommand = new RelayCommand<IDraggable>(x => x?.DragMove());
+        }
+    }
+}
---
+++ Views/FlashcardsSetWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Memento.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Memento.Views
+{
+    /// <summary>
+    /// Interaction logic for FlashcardsSetWindow.xaml
+    /// </summary>
+    public partial class FlashcardsSetWindow : Window, IClosable, IDraggable
+    {
+        public FlashcardsSetWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
