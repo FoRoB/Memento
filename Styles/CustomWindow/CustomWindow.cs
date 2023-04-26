7a415e78288fb37e698bfd526269e813bb1e67a3
FoRoB <andmyag190@yandex.ru>
2023-04-26 18:46:34 +0300

    created custom window style

---
+++ Styles/CustomWindow/CustomWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Memento.Styles.CustomWindow
+{
+    internal static class LocalExtensions
+    {
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+    }
+
+    public partial class CustomWindow
+    {
+        public void CloseButtonClick(object s, RoutedEventArgs e)
+        {
+            s.ForWindowFromTemplate(w => w.Close());
+        }
+
+        public void MinimizeButtonClick(object s, RoutedEventArgs e)
+        {
+            s.ForWindowFromTemplate(w => w.WindowState = WindowState.Minimized);
+        }
+
+        public void HeaderMouseLeftButtonDown(object s, MouseButtonEventArgs e)
+        {
+            s.ForWindowFromTemplate(w => w.DragMove());
+        }
+    }
+}
